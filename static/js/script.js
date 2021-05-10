3d3840a4a6617034e5c9efac30e26b7b97c1f175
DinaPapsujevica <dina.papsujevica@gmail.com>
2021-05-10 11:55:11 +0000

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     $(function(){
-        $( "#datepicker" ).datepicker();
+        $( "#datepicker" ).datepicker({
+            dateFormat: 'dd-mm-yy',
+            maxDate: '+3d',
+            minDate: '-3d'
+        });
     });
   });
